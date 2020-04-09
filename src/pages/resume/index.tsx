8dd90fbc45b4c16d7fd0c1fef3e5d2da9d4da22c
Dung Trinh <dungtm112@outlook.com>
2020-04-09 22:05:47 +0700

    [RESUME] - Add margin-top avatar

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -94,7 +94,7 @@ const HomePagePages = React.memo(() => {
                     />
                 </div>
                 <div className="col-md-4 c-main-avatar">
-                    <div className="row justify-content-center">
+                    <div className="row justify-content-center mt-4">
                         <img
                             src="https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-9/84634756_1853236188141892_8909589263876620288_o.jpg?_nc_cat=105&_nc_sid=85a577&_nc_ohc=8hon9Zw00LkAX9Tmw6l&_nc_ht=scontent-sin6-2.xx&oh=b5b9d48db180b03d5c44de3d484ecc70&oe=5EB17B5F"
                             width="90%"
